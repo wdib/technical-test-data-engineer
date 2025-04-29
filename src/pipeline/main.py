edd91c4191bd1924e953dc522d70471bf0cc9069
William Dib <info@williamdib.com>
2025-04-28 20:23:58 -0400

    Added pagination to /tracks endpoint data collection

--- src/pipeline/main.py
+++ src/pipeline/main.py
@@ -2,11 +2,20 @@ import asyncio
 import aiohttp
 
 async def main():
+    base_url = 'http://127.0.0.1:8000/tracks'
+    page = 1
+    all_list = []
     async with aiohttp.ClientSession() as session:
-        async with session.get( 'http://127.0.0.1:8000/tracks' ) as response:
-            print( 'Status', response.status )
-            fetch_json = await response.json()
-            print( 'Body', fetch_json )
+        while True:
+            params = { 'page' : page }
+            async with session.get( base_url, params=params ) as response:
+                print( f'Page {page}, Status {response.status}' )
+                fetch_json = await response.json()
+                fetch_list = fetch_json[ 'items' ]
+                all_list.extend( fetch_list )
+                if page == fetch_json[ 'pages' ]:
+                    break;
+                page += 1
 
 if __name__ == '__main__':
     asyncio.run(main())
