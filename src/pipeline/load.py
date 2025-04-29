fa88dedaa17687661bdef53b62d4893e6b462810
William Dib <info@williamdib.com>
2025-04-29 02:04:01 -0400

    Renamed 'filename' to 'endpoint' in load() for clarity

--- src/pipeline/load.py
+++ src/pipeline/load.py
@@ -3,10 +3,10 @@ import json
 from pathlib import Path
 from datetime import datetime
 
-async def load( data, filename ):
+async def load( data, endpoint ):
     data_dir = Path( 'data' )
     data_dir.mkdir( exist_ok=True )
     timestamp = datetime.now().strftime( '%Y_%m_%d_%H_%M_%S' )
-    file_path = data_dir / f'{timestamp}-{filename}.json'
+    file_path = data_dir / f'{timestamp}-{endpoint}.json'
     async with aiofiles.open( file_path, 'w' ) as f:
         await f.write( json.dumps( data, ensure_ascii=False, indent=4 ) )
