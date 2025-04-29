1d64f7eae9ee2bcace8ed3f17415eeeb00044e44
William Dib <info@williamdib.com>
2025-04-28 20:08:25 -0400

    Migrated FastAPI Pagination to v0.13.x as Page.with_custom_options class method was removed

--- src/moovitamix_fastapi/main.py
+++ src/moovitamix_fastapi/main.py
@@ -3,11 +3,15 @@ from fastapi import FastAPI, Query
 from fastapi.openapi.docs import get_swagger_ui_html
 from fastapi.responses import RedirectResponse
 from fastapi_pagination import Page, add_pagination, paginate
+from fastapi_pagination.customization import CustomizedPage, UseParamsFields
 from generate_fake_data import FakeDataGenerator
+from typing import TypeVar
 
-Page = Page.with_custom_options(
-    size=Query(100, ge=1, le=100),
-)
+T = TypeVar('T')
+Page = CustomizedPage[
+    Page[T],
+    UseParamsFields(size=Query(100, ge=1, le=100)),
+]
 
 app = FastAPI(
     title="MooVitamix",
