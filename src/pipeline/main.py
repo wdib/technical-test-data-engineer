a3cb80a027ea40c5abcf1aef31cdfc3175c85663
William Dib <info@williamdib.com>
2025-04-28 20:51:42 -0400

    Added basic error handling for HTTP response codes >=400

--- src/pipeline/main.py
+++ src/pipeline/main.py
@@ -9,13 +9,18 @@ async def main():
         while True:
             params = { 'page' : page }
             async with session.get( base_url, params=params ) as response:
-                print( f'Page {page}, Status {response.status}' )
-                fetch_json = await response.json()
-                fetch_list = fetch_json[ 'items' ]
-                all_list.extend( fetch_list )
-                if page == fetch_json[ 'pages' ]:
-                    break;
-                page += 1
+                try:
+                    response.raise_for_status()
+                    print( f'Page {page}, Status {response.status}' )
+                    fetch_json = await response.json()
+                    fetch_list = fetch_json[ 'items' ]
+                    all_list.extend( fetch_list )
+                    if page == fetch_json[ 'pages' ]:
+                        break;
+                    page += 1
+                except aiohttp.ClientResponseError as e:
+                    print( f'Request failed: {e.status} {e.message}' )
+    return all_list
 
 if __name__ == '__main__':
     asyncio.run(main())
